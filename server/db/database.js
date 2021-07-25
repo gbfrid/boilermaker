e74f8a29ea325b8d53115c1d0e88e5782e8d0c4a
gbfrid <gabefridkis@gmail.com>
2021-07-25 16:11:04 -0400

    whatever

--- server/db/database.js
+++ server/db/database.js
@@ -1,7 +1,14 @@
 const Sequelize = require('sequelize');
+const { config } = require('webpack');
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/boilermaker', {
-  logging: false
-});
+if(process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+}
+
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/boilermaker', config);
 
 module.exports = db;
